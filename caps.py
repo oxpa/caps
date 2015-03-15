e01ce15b289b1eb6da095a58ef74cb69333d26aa
oxpa <iippolitov@gmail.com>
2015-03-15 20:55:42 +0000

    simple voting added

--- caps.py
+++ caps.py
@@ -4,16 +4,16 @@ import sys
 sys.path.insert(0, '/usr/lib/python2.6/site-packages/Jinja2-2.6-py2.6.egg')
 
 import mysql.connector
-
 import MySQLdb
 
+import hashlib
 from datetime import datetime
-from flask import json, redirect, url_for, jsonify, Response, request, render_template
+from flask import json, redirect, url_for, jsonify, Response, request, render_template, make_response
 
 from flask import Flask
 app = Flask(__name__)
-#app.debug=True
-app.debug=False
+app.debug=True
+#app.debug=False
 import logging
 log=app.logger
 
@@ -24,10 +24,27 @@ log=app.logger
 #    return redirect(url_for('show_caps', page=0, page_size=10))
 
 
+def record_vote(ua='', ip='', cdate='', vote='', db='caps', host='localhost', user='caps', pwd='caps'):
+    try:
+        db=MySQLdb.connect(host=host, user=user, passwd=pwd, db=db, use_unicode=True, charset='utf8')
+        c=db.cursor()
+        stored_ua = hashlib.sha512(ua).hexdigest() 
+        log.debug('voting with : hash %s, date %s, ip %s, count %s', stored_ua, cdate, ip, vote)
+        stmt_log = """insert into votes (votes_uahash, votes_capsdate, votes_ip, votes_count) 
+                                values (%s, %s, %s, %s) 
+                on duplicate key update 
+                    votes_count = votes_count + values(votes_count)"""
+        res = c.execute(stmt_log,(stored_ua, cdate, ip, vote))
+        stmt_update = 'update caps set caps_votes = caps_votes + %s where caps_date = %s'
+        res = c.execute(stmt_update,(vote, cdate))
+        db.commit()
+    except Exception as e:
+        log.warning('an exception occured from ip %s, voting using %s, for caps %s ,vote %s : %s', ip, ua, cdate, vote, e)
+ 
 def get_caps(page=None, page_size=10, db='caps', host='localhost', user='caps', pwd='caps', cf_id = 1):
     db=MySQLdb.connect(host=host, user=user, passwd=pwd, db=db, use_unicode=True, charset='utf8')
     c=db.cursor()
-    stmt = "select caps_date, caps_author, caps_text from caps where caps_conference = %s order by caps_date asc limit %s, %s"
+    stmt = "select caps_date, caps_author, caps_text, caps_votes from caps where caps_conference = %s order by caps_date asc limit %s, %s"
     pre_stmt = "select cstats_totalcaps div %s, cstats_totalcaps mod %s + %s from confstats where cstats_confid = %s"
     c.execute(pre_stmt,(page_size, page_size, page_size, cf_id))
     log.debug(pre_stmt, page_size, page_size, page_size, cf_id)
@@ -42,7 +59,7 @@ def get_caps(page=None, page_size=10, db='caps', host='localhost', user='caps', 
     log.debug("limit start is %s, limit size is %s", limit_start, limit_size)
 
     c.execute(stmt, (cf_id, limit_start, limit_size))
-    rval = [ {'date':i[0]/1000, 'us': ('%03d'%(i[0]%1000)).rstrip('0') ,  'author':i[1], 'text':i[2].replace('\n', '<br/>')} for i in c.fetchall() ]
+    rval = [ {'date':i[0]/1000, 'us': ('%03d'%(i[0]%1000)).rstrip('0') ,  'author':i[1], 'text':i[2].replace('\n', '<br/>'), 'votes':i[3]} for i in c.fetchall() ]
     rval.reverse()
     return {'pagestotal':total_pages, 'caps':rval }
 
@@ -52,7 +69,7 @@ def get_caps_by_date(date=None, db='caps', host='localhost', user='caps', pwd='c
 
     db=MySQLdb.connect(host=host, user=user, passwd=pwd, db=db, use_unicode=True, charset='utf8')
     c=db.cursor()
-    stmt = "select caps_date, caps_author, caps_text, twitter_nick from caps left join authors on caps_author = authors_nick left join twitter on author_real_id = authors_real_id where caps_conference = %s and caps_date like %s limit 1"
+    stmt = "select caps_date, caps_author, caps_text, caps_votes, twitter_nick from caps left join authors on caps_author = authors_nick left join twitter on author_real_id = authors_real_id where caps_conference = %s and caps_date like %s limit 1"
     #stmt = "select caps_date, caps_author, caps_text, coalesce(twitter_nick, caps_author) from caps join authors on caps_author = authors_nick left join twitter on author_real_id = authors_real_id where caps_conference = %s and caps_date like %s limit 1"
     pre_stmt = "select cstats_totalcaps div %s, cstats_totalcaps mod %s + %s from confstats where cstats_confid = %s"
     page_size = 10
@@ -61,7 +78,7 @@ def get_caps_by_date(date=None, db='caps', host='localhost', user='caps', pwd='c
     log.debug('date is %s', date)
     c.execute(stmt, (cf_id, '%s%%'%date))
 
-    rval = [ {'date':i[0]/1000, 'us': ('%03d'%(i[0]%1000)).rstrip('0') ,  'author':i[1], 'text':i[2].replace('\n', '<br/>'), 'twauthor':i[3]} for i in c.fetchall() ]
+    rval = [ {'date':i[0]/1000, 'us': ('%03d'%(i[0]%1000)).rstrip('0') ,  'author':i[1], 'text':i[2].replace('\n', '<br/>'), 'votes':i[3], 'twauthor':i[4]} for i in c.fetchall() ]
     rval.reverse()
     return {'pagestotal':total_pages, 'caps':rval }
 
@@ -101,6 +118,20 @@ def show_caps(page=None, page_size=10):
     else:
         log.debug('useragent is %s, rule is %s', request.user_agent.string, request.url_rule.rule)
         return Response(json.dumps(caps, ensure_ascii=False),  content_type='application/json; charset=utf-8')
+
+@app.route('/vote/',methods=['GET','POST'])
+@app.route('/vote/<int:cdate>/', methods=['GET','POST'])
+def vote(cdate=None):
+    if request.method == 'POST' and cdate != None and request.cookies.get('%s'%cdate) == None and request.json != None:
+        log.debug('voting for %s, cookies is %s, vote is %s', cdate, request.cookies.get('%s'%cdate), request.json.get('vote'))
+        #log.debug(request.json.get('vote'))
+        resp = make_response('',204)
+        resp.set_cookie(cdate, 'v', max_age=60*60*24)
+        record_vote(request.user_agent.string, request.headers.get('X-Real-IP'), cdate, request.json.get('vote'))
+        return resp
+         
+        
+    return '', 418
 
 if __name__ == '__main__':
     app.config['SERVER_NAME'] = 'localhost:5000'
